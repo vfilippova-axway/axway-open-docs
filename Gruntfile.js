50375d59d523aad3e0fe93ce2074ff415bd6a22b
Uzair Ali <uzaira960@gmail.com>
2019-09-13 12:39:20 +0100

    Added a task to the gruntfile that checks if http links are dead links

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  const checkLinks = require('md-link-checker')
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -20,11 +22,53 @@ module.exports = function(grunt) {
       src: [
         'content/en/**/*.md'
       ]
+    },
+
+    md_link_checker: {
+      src: ['content/en/**/*.md']
     }
   });
 
-  grunt.loadNpmTasks('grunt-markdownlint');
+  // grunt.loadNpmTasks('grunt-markdownlint');
+  grunt.registerTask('default', ['md_link_checker']);
 
-  grunt.registerTask('default', ['markdownlint']);
+  grunt.registerMultiTask('md_link_checker', 'Check links in markdown even github private ones', function () {
+    const done = this.async();
+    const options = this.options({
+      tokenFilePath: undefined,
+  });
 
+  this.files.forEach(function (f) {
+    const src = f.src.filter(function (path) {
+      if (!grunt.file.exists(path)) {
+        grunt.log.warn(`File ${path} not found.`);
+        return false;
+      } else {
+        return true;
+      }
+    });
+
+    const config = {
+      files: src,
+      tokenFilePath: options.tokenFilePath
+    };
+
+    return checkLinks(config).then(function (data) {
+      grunt.log.ok('No dead links found');
+      return done();
+    }).catch(function (data) {
+      data.forEach(function(dead) {
+        dead.deadlinks.forEach(function(deadlink) {
+          // only fail if the dead link is a http link
+          if (deadlink.includes('http')) {
+            grunt.log.warn(`Link ${deadlink} in ${dead.filename} not found.`);
+            return done(false);
+          } else {
+            return done();
+          }
+        })
+      })
+    });
+  });
+});
 };
